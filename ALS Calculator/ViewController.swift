a9745b092ab1d8e999dc0c8bd8863f08382a0662
Cassandra Lam <cassandralam@gmail.com>
2018-05-11 18:00:29 -0600

    Initial Commit

---
+++ ALS Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ALS Calculator
+//
+//  Created by Cassandra Lam on 2018-05-11.
+//  Copyright © 2018 Cassandra Lam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
