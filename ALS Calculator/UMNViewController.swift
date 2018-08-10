d37722d28cbfe0d480347b944941609cf6f4348e
Cassandra Lam <cassandralam@gmail.com>
2018-08-09 18:34:07 -0600

    Created typeOfSwitch enum so each switch type (bulb, cerv, thor...) represents an Int value and is used to index the symptomArray. The symptomArray holds all current bool states of the switches on that view controller.

--- ALS Calculator/UMNViewController.swift
+++ ALS Calculator/UMNViewController.swift
@@ -8,11 +8,27 @@
 
 import UIKit
 
+enum typeOfSwitch : Int{
+    case Bulb, Cerv, Thor, Lum, Gene, Ros
+}
+
+func checkSwitch (switchOutlet: UISwitch, symptomArray: inout [Bool], symptomLocation: typeOfSwitch){
+    //Global function that checks switch state and stores that value in desired array that holds all switch values for that view controller
+    
+    if switchOutlet.isOn == true {
+        symptomArray[symptomLocation.rawValue] = true   //depending on switch, bool value is placed at corresponding array index
+        print(symptomArray)
+    }
+    else{
+        symptomArray[symptomLocation.rawValue] = false
+        print(symptomArray)
+    }
+}
+
 class UmnViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
 
@@ -20,5 +36,41 @@ class UmnViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+//Store current switch bool states in an array designated for the umn view controller only:
+    var umnSwitchStates = [Bool](repeating: false, count: 6) //6 switches total on UMN view controller
     
+    @IBOutlet weak var umnBulbOutlet: UISwitch!
+    @IBAction func umnBulbAction(_ sender: UISwitch) {
+        checkSwitch(switchOutlet: umnBulbOutlet, symptomArray: &umnSwitchStates, symptomLocation: typeOfSwitch.Bulb)   //stores the current bulbar switch bool state to array umnSwitchStates
+    }
+    
+    
+    
+    @IBOutlet weak var umnCervOutlet: UISwitch!
+    @IBAction func umnCervAction(_ sender: UISwitch) {
+        checkSwitch(switchOutlet: umnCervOutlet, symptomArray: &umnSwitchStates, symptomLocation: typeOfSwitch.Cerv)
+    }
+
+    @IBOutlet weak var umnThorOutlet: UISwitch!
+    @IBAction func umnThorAction(_ sender: UISwitch) {
+        checkSwitch(switchOutlet: umnThorOutlet, symptomArray: &umnSwitchStates, symptomLocation: typeOfSwitch.Thor)
+    }
+
+    @IBOutlet weak var umnLumOutlet: UISwitch!
+    @IBAction func umnLumAction(_ sender: UISwitch) {
+        checkSwitch(switchOutlet: umnLumOutlet, symptomArray: &umnSwitchStates, symptomLocation: typeOfSwitch.Lum)
+    }
+
+    @IBOutlet weak var umnGeneOutlet: UISwitch!
+    @IBAction func umnGeneAction(_ sender: UISwitch) {
+        checkSwitch(switchOutlet: umnGeneOutlet, symptomArray: &umnSwitchStates, symptomLocation: typeOfSwitch.Gene)
+    }
+
+    @IBOutlet weak var umnRosOutlet: UISwitch!
+    @IBAction func umnRosAction(_ sender: UISwitch) {
+        checkSwitch(switchOutlet: umnRosOutlet, symptomArray: &umnSwitchStates, symptomLocation: typeOfSwitch.Ros)
+    }
+
+
 }
