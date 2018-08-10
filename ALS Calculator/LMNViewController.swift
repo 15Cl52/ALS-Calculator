9046e6cd5b1563d04a86707cb79e1b9facf4aca1
Cassandra Lam <cassandralam@gmail.com>
2018-08-09 18:31:30 -0600

    Deleted and readded LmnViewController class because of error linking switch from storyboard to the view controller (error said LmnViewController class did not exist).

--- ALS Calculator/LMNViewController.swift
+++ ALS Calculator/LMNViewController.swift
@@ -2,34 +2,48 @@
 //  LmnViewController.swift
 //  ALS Calculator
 //
-//  Created by Cassandra Lam on 2018-07-13.
+//  Created by Cassandra Lam on 2018-08-09.
 //  Copyright © 2018 Cassandra Lam. All rights reserved.
 //
 
 import UIKit
 
 class LmnViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+//Store current switch bool states in an array designated for the umn view controller only:
+    
+    var lmnSwitchStates: [Bool] = []
+    
+    @IBOutlet weak var lmnBulbOutlet: UISwitch!
+    @IBAction func lmnBulbAction(_ sender: UISwitch) {
+        checkSwitch(switchOutlet: lmnBulbOutlet, symptomArray: &lmnSwitchStates, symptomLocation: typeOfSwitch.Bulb)
     }
-    */
-
+    
+    @IBOutlet weak var lmnCervOutlet: UISwitch!
+    @IBAction func lmnCervAction(_ sender: UISwitch) {
+        checkSwitch(switchOutlet: lmnCervOutlet, symptomArray: &lmnSwitchStates, symptomLocation: typeOfSwitch.Cerv)
+    }
+    
+    @IBOutlet weak var lmnThorOutlet: UISwitch!
+    @IBAction func lmnThorAction(_ sender: UISwitch) {
+        checkSwitch(switchOutlet: lmnThorOutlet, symptomArray: &lmnSwitchStates, symptomLocation: typeOfSwitch.Thor)
+    }
+    
+    @IBOutlet weak var lmnLumbOutlet: UISwitch!
+    @IBAction func lmnLumbAction(_ sender: UISwitch) {
+        checkSwitch(switchOutlet: lmnLumbOutlet, symptomArray: &lmnSwitchStates, symptomLocation: typeOfSwitch.Lum)
+    }
+    
 }
